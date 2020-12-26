7c587183b44c9447589ff22f417184557a47fd1b
Rahul <developerrahul55@gmail.com>
2020-12-26 13:10:18 +0530

    Importing project

---
+++ MVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return  typeof(Controller).Assembly.GetName().Version.ToString()+ "Hello this is Rahul Bhandari!!";
+             
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your Application description Page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact Page.";
+
+            return View();
+        }
+    }
+}
